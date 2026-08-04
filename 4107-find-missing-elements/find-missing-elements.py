6ae83b11d989e0050c9ba7fe9d0ab1b600cbadb5
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2026-08-04 11:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.85%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        ans = []
+        for x, y in pairwise(nums):
+            ans.extend(range(x + 1, y))
+        return ans
+        
